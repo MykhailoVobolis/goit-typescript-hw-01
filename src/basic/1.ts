35c3b31365243d395144dc0ad38642642dc1f0a2
MykhailoVobolis <mvobolis@gmail.com>
2024-05-27 15:23:01 +0300

    add task list

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -1,11 +1,3 @@
-// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
-
-// const age = 50;
-// const username = 'Max';
-// const toggle = true;
-// const empty = null;
-// const callback = (a) => { return 100 + a };
-
 const age: number = 50;
 const username: string = "Max";
 const toggle: boolean = true;
--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,19 +1,3 @@
-// У вас є такі функції JavaScript:
-
-// function showMessage(message) {
-//   console.log(message);
-// }
-
-// function calc(num1, num2) {
-//   return num1 + num2;
-// }
-
-// function customError() {
-//   throw new Error("Error");
-// }
-
-// Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
-
 function showMessage(message: string): void {
   console.log(message);
 }
